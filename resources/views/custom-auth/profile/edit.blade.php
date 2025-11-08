9228377a12d7ce7885ac817ab15653dd19927564
x0u6x <la.t62.xx@gmail.com>
2025-11-08 12:29:42 +0900

    feat: custom-authに各分岐を追加 -tailwind除去済み

--- resources/views/custom-auth/profile/edit.blade.php
+++ resources/views/custom-auth/profile/edit.blade.php
@@ -10,18 +10,18 @@
     <!-- プロフィール情報更新 -->
     <section class="profile-section">
         <h2>基本情報</h2>
-        @include('auth.profile.partials.update-profile-information-form')
+        @include('custom-auth.profile.partials.update-profile-information-form')
     </section>
 
     <!-- パスワード更新 -->
     <section class="profile-section">
         <h2>パスワード変更</h2>
-        @include('auth.profile.partials.update-password-form')
+        @include('custom-auth.profile.partials.update-password-form')
     </section>
 
     <!-- アカウント削除 -->
     <section class="profile-section delete-section">
         <h2>アカウント削除</h2>
-        @include('auth.profile.partials.delete-user-form')
+        @include('custom-auth.profile.partials.delete-user-form')
     </section>
 </div>
---
+++ resources/views/custom-auth/profile/partials/delete-user-form.blade.php
@@ -0,0 +1,53 @@
+<section>
+    <header>
+        <h2>
+            {{ __('Delete Account') }}
+        </h2>
+
+        <p>
+            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
+        </p>
+    </header>
+
+    <x-danger-button
+        x-data=""
+        x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')">{{ __('Delete Account') }}</x-danger-button>
+
+    <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
+        <form method="post" action="{{ route('profile.destroy') }}">
+            @csrf
+            @method('delete')
+
+            <h2>
+                {{ __('Are you sure you want to delete your account?') }}
+            </h2>
+
+            <p>
+                {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+            </p>
+
+            <div>
+                <x-input-label for="password" value="{{ __('Password') }}" />
+
+                <x-text-input
+                    id="password"
+                    name="password"
+                    type="password"
+
+                    placeholder="{{ __('Password') }}" />
+
+                <x-input-error :messages="$errors->userDeletion->get('password')" />
+            </div>
+
+            <div>
+                <x-secondary-button x-on:click="$dispatch('close')">
+                    {{ __('Cancel') }}
+                </x-secondary-button>
+
+                <x-danger-button>
+                    {{ __('Delete Account') }}
+                </x-danger-button>
+            </div>
+        </form>
+    </x-modal>
+</section>
