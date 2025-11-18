ec7f2586c8325c89fd89250f8d970f5ab46ba341
x0u6x <la.t62.xx@gmail.com>
2025-11-18 15:34:53 +0900

    style:class書いて見た目整えた

--- resources/views/custom-auth/profile/partials/update-profile-information-form.blade.php
+++ resources/views/custom-auth/profile/partials/update-profile-information-form.blade.php
@@ -1,13 +1,8 @@
 <section>
-    <header>
-        <h2>
-            {{ __('Profile Information') }}
-        </h2>
 
-        <p>
-            {{ __("Update your account's profile information and email address.") }}
-        </p>
-    </header>
+    <h2>メールアドレス変更</h2>
+    <p class="profile-info">アカウントに登録されているメールアドレスを変更できます。</p>
+
 
     <form id="send-verification" method="post" action="{{ route('verification.send') }}">
         @csrf
@@ -18,44 +13,29 @@
         @method('patch')
 
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" :value="old('name', $user->name)" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" />
+            <label for="email" class="form-label">メールアドレス</label>
+
+            <input
+                id="email" name="email" type="email"
+                value="{{ old('email', $user->email) }}"
+                required autocomplete="username"
+                class="form-input" />
+
+            @error('email')
+            <p class="form-error">{{ $message }}</p>
+            @enderror
         </div>
 
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" name="email" type="email" :value="old('email', $user->email)" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" />
-
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-            <div>
-                <p>
-                    {{ __('Your email address is unverified.') }}
-
-                    <button form="send-verification">
-                        {{ __('Click here to re-send the verification email.') }}
-                    </button>
-                </p>
-
-                @if (session('status') === 'verification-link-sent')
-                <p>
-                    {{ __('A new verification link has been sent to your email address.') }}
-                </p>
-                @endif
-            </div>
-            @endif
-        </div>
-
-        <div>
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <button type="submit" class="profile-save-btn">保存する</button>
 
             @if (session('status') === 'profile-updated')
             <p
+                class="profile-saved-message"
                 x-data="{ show: true }"
                 x-show="show"
                 x-transition
-                x-init="setTimeout(() => show = false, 2000)">{{ __('Saved.') }}</p>
+                x-init="setTimeout(() => show = false, 2000)">保存しました</p>
             @endif
         </div>
     </form>
