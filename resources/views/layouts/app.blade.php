ab96902bc935788762e817239817739e2c891327
x0u6x <la.t62.xx@gmail.com>
2025-11-19 15:39:55 +0900

    feat:modal.cssを追記

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') - Brush Edit</title>
 
-    @vite(['resources/css/reset.css']) <!--リセットCSS -->
-    @vite(['resources/css/directory.css']) <!--このページのみのCSS -->
-    @vite(['resources/css/profile.css']) <!--このページのみのCSS -->
-    @vite(['resources/css/pagination.css']) <!--ページネーション -->
+    @vite(['resources/css/reset.css']) <!-- リセットCSS -->
+    @vite(['resources/css/directory.css']) <!-- ユーザーホーム画面 -->
+    @vite(['resources/css/profile.css']) <!-- アカウント管理画面 -->
+    @vite(['resources/css/modal.css']) <!-- モーダル -->
+    @vite(['resources/css/pagination.css']) <!-- ページネーション -->
 
     @vite(['resources/js/app.js'])
 </head>
