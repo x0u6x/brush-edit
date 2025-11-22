cd2a53ac9081ce34aea7562193daf874247d3e59
x0u6x <la.t62.xx@gmail.com>
2025-11-22 12:29:26 +0900

    feat:パスワードリセットモーダルのみ分割

---
+++ resources/views/custom-auth/modals/password-reset-modal.blade.php
@@ -0,0 +1,32 @@
+<x-modals.modal-wrapper :open="'openPasswordReset'">
+
+    <x-slot:trigger>
+
+        <a href="#"
+            class="forgot-password-link"
+            @click.prevent="openPasswordReset = true">
+            パスワードをお忘れですか？
+        </a>
+
+    </x-slot:trigger>
+
+    <h2 class="modal-title">パスワードリセット</h2>
+    <p class="modal-text">
+        ご登録時のメールアドレスを入力してください。<br>
+        リセットリンクを送信します。</p>
+
+    <form method="POST" action="{{ route('password.email') }}">
+        @csrf
+
+        <label for="reset-email">メールアドレス</label>
+        <input id="reset-email" type="email" name="email" class="input-text">
+        @error('email')
+        <p class="form-error">{{ $message }}</p>
+        @enderror
+
+        <button type="submit" class="btn-nomal">
+            リセットリンクを送信
+        </button>
+    </form>
+
+</x-modals.modal-wrapper>
